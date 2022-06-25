2b78c2af3fc3f57219afcd05b0aee09e2c32259a
Robin Delbaere <hello@rdelbaere.fr>
2022-06-25 17:41:13 +0200

    Guard strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SystemComponent } from "./system/views/system/system.component";
 import { LoginComponent } from "./system/views/login/login.component";
+import { AuthenticatedGuard } from "./system/guards/authenticated.guard";
+import { UnauthenticatedGuard } from "./system/guards/unauthenticated.guard";
 
 const routes: Routes = [
-    { path: '', component: SystemComponent },
-    { path: 'login', component: LoginComponent },
+    { path: '', component: SystemComponent, canActivate: [AuthenticatedGuard] },
+    { path: 'login', component: LoginComponent, canActivate: [UnauthenticatedGuard] },
 ];
 
 @NgModule({
