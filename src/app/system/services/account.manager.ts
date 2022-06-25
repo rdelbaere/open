fdbbcd99aaedf679130a9cffec7a1f32ad839ced
Robin Delbaere <hello@rdelbaere.fr>
2022-06-25 17:51:45 +0200

    Store session

--- src/app/system/services/account.manager.ts
+++ src/app/system/services/account.manager.ts
@@ -3,26 +3,44 @@ import { Observable } from "rxjs";
 import { catchError, map } from 'rxjs/operators';
 import { BackendService } from "./backend.service";
 import { Credentials } from "../interfaces/backend/credentials";
+import { AccountSession } from "../interfaces/core/account";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AccountManager {
-    private token: string;
+    private session: AccountSession;
+    private storageKey: string = 'open-session';
 
-    constructor(private backend: BackendService){}
+    constructor(private backend: BackendService){
+        this.reloadSession();
+    }
 
     authenticate(credentials: Credentials): Observable<boolean>{
         return this.backend.post('/auth', credentials)
             .pipe(
-                map(payload => {
-                    this.token = payload.data.token;
-                    return true;
-                })
+                map(payload => this.startSession(payload.data))
             );
     }
 
     isAuthenticated(): boolean{
-        return !!this.token;
+        return !!this.session;
+    }
+
+    private startSession(session: AccountSession): boolean{
+        this.session = session;
+        this.storeSession();
+        return true;
+    }
+
+    private storeSession(){
+        localStorage.setItem(this.storageKey, JSON.stringify(this.session))
+    }
+
+    private reloadSession(){
+        const stored = localStorage.getItem(this.storageKey);
+        if(stored){
+            this.session = JSON.parse(stored);
+        }
     }
 }
